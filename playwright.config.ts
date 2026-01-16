81d007f844b6fc5af29fa3b5bed778085725f237
Ломова Александра Александровна <a.a.lomova@tbank.ru>
2026-01-16 20:18:25 +0700

    4 commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  reporter: [['html', { open: 'never' }]],
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['line'], ['allure-playwright']],
+  //reporter: [['line'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
